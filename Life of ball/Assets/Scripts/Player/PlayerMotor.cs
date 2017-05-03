0eb8bd1b45ce9dba5fbb5525e45e04b8db9c1345
epain17 <epain17@gmail.com>
2017-05-03 22:11:16 +0200

    #7 Jump works
    The current script dosen't use rigyd body but the old script still
    exists so might find a way to solve the problem later

---
+++ Life of ball/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private CharacterController controller;
+
+    private float verticalVelocity;
+    private float horizontalVelocity;
+    private float zVelocity;
+    private float gravity = 14.0f;
+    private float jumpForce = 10.0f;
+
+    Vector3 moveVector;
+
+    public float speed = 6f;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+     
+    }
+
+    private void Update()
+    {
+        horizontalVelocity = Input.GetAxis("Horizontal");
+        zVelocity = Input.GetAxis("Vertical");
+        if (controller.isGrounded)
+        {
+            
+            verticalVelocity = -gravity * Time.deltaTime;
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+            
+        }
+
+        moveVector = new Vector3(horizontalVelocity * speed, verticalVelocity, zVelocity * speed);
+        moveVector = Camera.main.transform.TransformDirection(moveVector);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+	
+}
