131864560471ce94698a75be3384c5f0c3a3da57
felixhelgesson <felix.helgesson@gmail.com>
2017-05-04 13:32:49 +0200

    #11 Animation
    Trying to do some triggered animation.

---
+++ Life of ball/Assets/Scripts/Enemies/HelmetFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelmetFollow : MonoBehaviour {
+
+    public GameObject Ball;       //Public variable to store a reference to the player game object
+
+    Vector3 offset;
+                                    // Use this for initialization
+    void Start ()
+    {
+        offset = new Vector3(0, 0.3f, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Ball.transform.position + offset;
+    }
+}
---
+++ Life of ball/Assets/TriggerBridgeAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBridgeAnim : MonoBehaviour {
+
+    public Animator bridgeAnim;
+    public Animator buttonAnim;
+
+	// Use this for initialization
+	void Start () {
+
+        bridgeAnim.enabled = false;
+        buttonAnim.enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    //void OnTriggerEnter()
+    //{
+    //    bridgeAnim.Play("Take 001");
+    //    buttonAnim.Play("Take 001");
+    //}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BridgeButton")
+        {
+            bridgeAnim.StartPlayback();
+            buttonAnim.StartPlayback();
+        }
+       
+    }
+}
