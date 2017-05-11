29b80f5bed83d82030a3f4df4a5386aa16b8f957
epain17 <epain17@gmail.com>
2017-05-11 14:26:36 +0200

    #18 Button works with plattforms

---
+++ Life of ball/Assets/Scripts/AnimationTriggers/AnimationTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+
+    public Animator buttonAnimator;
+    public Animator armsAnimator;
+    public GameObject[] arms;
+
+
+
+
+    void Start()
+    {
+        buttonAnimator = GetComponent<Animator>();
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            buttonAnimator.Play("ButtonDown");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        buttonAnimator.Play("ButtonUp");
+        foreach (GameObject g in arms)
+        {
+            armsAnimator = g.GetComponent<Animator>();
+            armsAnimator.Play("ArmExtend");
+            new WaitForSecondsRealtime(2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("q"))
+        {
+            buttonAnimator.Play("ButtonDown");
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            buttonAnimator.Play("ButtonUp");
+        }
+
+    }
+}
---
+++ Life of ball/Assets/Scripts/AnimationTriggers/BridgeTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTrigger : MonoBehaviour
+{
+
+
+    public Animator buttonAnimator;
+    public Animator bridgeAnimator;
+
+
+    void Start()
+    {
+        buttonAnimator = GetComponent<Animator>();
+        bridgeAnimator.Play("IdleFolded");
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            buttonAnimator.Play("ButtonDown");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        buttonAnimator.Play("ButtonUp");
+        bridgeAnimator.Play("ArmExtend");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("q"))
+        {
+            buttonAnimator.Play("ButtonDown");
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            buttonAnimator.Play("ButtonUp");
+        }
+
+    }
+}
